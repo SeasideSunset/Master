00e95037389f4b5398338ba0edc9eaea70f0587c
SeasideSunset <87993622+SeasideSunset@users.noreply.github.com>
2021-07-26 12:18:42 -0400

    Create ApiFunctions.lua

---
+++ ApiFunctions.lua
@@ -0,0 +1,50 @@
+--Made by Riot / Falcon / Proxide
+
+local _vehiclestatus = game.Players.LocalPlayer.Character:FindFirstChild('InVehicle')
+
+local function sendnotification(args1, args2)
+	require(game:GetService("ReplicatedStorage").Game.Notification).new({
+		Text = args1,
+		Duration = args2
+	})
+end
+
+local function equipitem(args)
+	local var
+	for i,v in pairs(getgc(true)) do
+		if typeof(v) == "table" and rawget(v, "i") and rawget(v, "Frame") and rawget(v, "Name") then
+			if v.Name == args then
+				var = v
+			end
+		end
+	end
+	require(game:GetService("ReplicatedStorage").Game.ItemSystem.ItemSystem).Equip(game:GetService("Players").LocalPlayer, var)
+end
+
+local function firecircleaction(args)
+	local UI = require(game:GetService("ReplicatedStorage").Module:WaitForChild("UI"));
+	for i,v in pairs (UI.CircleAction.Specs) do
+		if v.Name == args then
+			v:Callback(v, true)
+		end
+	end
+end
+
+local function spawnvehicle(args)
+    for i, v in pairs(getgc(true)) do
+        if type(v) == 'function' then
+                if getfenv(v).script == game:GetService('ReplicatedStorage').Game.Garage.GarageUI then
+                local con = getconstants(v)
+                if table.find(con, 'Type') and table.find(con, 'Make') and table.find(con, 'FireServer') then
+                    SpawnVehicle = v
+                end
+            end
+        end
+    end
+    
+    for i, v in pairs(require(game:GetService('ReplicatedStorage').Game.Garage.VehicleData)) do
+        if v.Make == args then
+            SpawnVehicle(v)
+        end
+    end
+end
