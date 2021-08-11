c09312c81215d9ea47320367b5a9773213d3f7c7
SeasideSunset <87993622+SeasideSunset@users.noreply.github.com>
2021-08-11 19:08:12 -0400

    Update ApiFunctions.lua

--- ApiFunctions.lua
+++ ApiFunctions.lua
@@ -1,6 +1,28 @@
 --Made by Riot / Falcon / Proxide
 
+--[[
+
+Update Log: Added findvehiclestatus() function, no arguments
+e.g.
+
+if not api:findvehiclestatus() then
+	api:sendnotification("You are not inside of a vehicle, failed to run!", 2)
+else
+	api:sendnotification("Player inside of a vehicle; Success!", 2)
+   end
+end
+
+]]--
+
 local api = {}
+
+function api:findvehiclestatus()
+	if game.Players.LocalPlayer.Character:FindFirstChild("InVehicle") ~= nil then
+    	return true
+    else
+    	return false
+    end
+end
 
 function api:sendnotification(args1, args2)
 	require(game:GetService("ReplicatedStorage").Game.Notification).new({
